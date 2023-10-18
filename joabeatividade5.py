d22bdb575dc62035c274ac668b4d01e258799e44
Joabe Jordão de Souza Junior <146821705+Joadejdsj@users.noreply.github.com>
2023-10-18 11:37:16 -0300

    Add files via upload

---
+++ joabeatividade5.py
@@ -0,0 +1,6 @@
+# Digite uma metragem e veja isso em cm e mm:
+print("Descubra a metragem de um némero em centímetremos e milímetros")
+numero = float(input("Digite um número: "))
+centimetros = numero/100
+milimetros = numero/1000
+print(f'O número em centímetros é: {centimetros}cm e o número em milímetros é: {milimetros}mm')
